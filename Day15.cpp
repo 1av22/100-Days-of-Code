8ec9cc265d2113eefd51962a621333eb9d33a0ae
Aditya Verma <adichichi162@gmail.com>
2023-06-21 00:02:17 +0530

    Day15 Code - final code

--- Day15.cpp
+++ Day15.cpp
@@ -4,11 +4,44 @@
 #include <iostream>
 #include <vector>
 
+void solve(std::vector<int> nums, std::vector<int> output, int index, std::vector<std::vector<int>> &answer)
+{
+    // Base case
+    if (index >= nums.size())
+    {
+        answer.push_back(output);
+        return;
+    }
+
+    // exclude
+    solve(nums, output, index + 1, answer);
+
+    // include
+    int element = nums[index];
+    output.push_back(element);
+    solve(nums, output, index + 1, answer);
+}
+
 std::vector<std::vector<int>> subsets(std::vector<int> &nums)
 {
     std::vector<std::vector<int>> answer;
+    std::vector<int> output;
+    int index = 0;
+    solve(nums, output, index, answer);
+    return answer;
 }
 
 int main()
 {
+    std::vector<int> nums = {1, 2, 3};
+    std::vector<std::vector<int>> ans = subsets(nums);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        int s = ans[i].size();
+        for (int j = 0; j < s; j++)
+        {
+            std::cout << ans[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
