3faeaf55ceb97ff9a333d5deadb72a437e92269b
Aditya Verma <adichichi162@gmail.com>
2023-06-20 23:47:03 +0530

    Day14 Code - initial code

---
+++ Day15.cpp
@@ -0,0 +1,14 @@
+// Day 15
+// Finding the power set of an array
+
+#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> subsets(std::vector<int> &nums)
+{
+    std::vector<std::vector<int>> answer;
+}
+
+int main()
+{
+}
